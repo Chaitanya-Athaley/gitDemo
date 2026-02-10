7ad67d338f9780547843d7fe95439a949968fe78
Chaitanya <55506745+Chaitanya-Athaley@users.noreply.github.com>
2026-02-10 23:13:51 +0530

    Add new print statement in HelloWorld.java
    Added a new print statement for online.

--- Basic-CMD-Test/HelloWorld.java
+++ Basic-CMD-Test/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Add new line");
+
+
+        System.out.println("Add new line for online");
+
     }
 }
