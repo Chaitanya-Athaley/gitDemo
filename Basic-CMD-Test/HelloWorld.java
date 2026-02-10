40c5361956c85b8a1627386c49ddf3a9c3d5a02f
Chaitanya <55506745+Chaitanya-Athaley@users.noreply.github.com>
2026-02-10 23:23:12 +0530

    Feature/102 (#3)
    * Merge pull request #1 from Chaitanya-Athaley/feature/101 (#2)
    
    * tr

--- Basic-CMD-Test/HelloWorld.java
+++ Basic-CMD-Test/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Add new line");
-
+        System.out.println("Add new line");
 
         System.out.println("Add new line for online");
 
